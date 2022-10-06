bfd88e035a7bcb919e6874839f04d33a650a5a84
Masl <70035395+marceldobehere@users.noreply.github.com>
2022-10-06 10:41:12 +0200

    aa

--- Masl Disk File Converter/Program.cs
+++ Masl Disk File Converter/Program.cs
@@ -70,6 +70,39 @@ namespace Masl_Disk_File_Converter
                 writer.WriteLine(totalSize);
                 writer.WriteLine("Partition Count:");
                 writer.WriteLine(partCount);
+                writer.WriteLine("List:");
+                for (int i = 0; i < partCount; i++)
+                {
+                    writer.WriteLine("Partition ID:");
+                    writer.WriteLine(i);
+                    
+                    ushort nameLen = BEC.ByteArrToUint16(diskData, offset);
+                    offset += 2;
+                    string name = string.Empty;
+                    for (int i2 = 0; i2 < nameLen; i2++)
+                        name += (char)diskData[offset++];
+                    writer.WriteLine("Name:");
+                    writer.WriteLine(name);
+
+                    ushort descLen = BEC.ByteArrToUint16(diskData, offset);
+                    offset += 2;
+                    string desc = string.Empty;
+                    for (int i2 = 0; i2 < descLen; i2++)
+                        desc += (char)diskData[offset++];
+                    writer.WriteLine("Description:");
+                    writer.WriteLine(desc);
+
+                    ushort driveNameLen = BEC.ByteArrToUint16(diskData, offset);
+                    offset += 2;
+                    string driveName = string.Empty;
+                    for (int i2 = 0; i2 < driveNameLen; i2++)
+                        driveName += (char)diskData[offset++];
+                    writer.WriteLine("Drive Name:");
+                    writer.WriteLine(driveName);
+
+                    bool hidden = diskData[offset++] == 1;
+                    
+                }
             }
 
 
